c02b41dd0ac8e968885a7f2069bddb691815f122
Tarbian <anyfuture@protonmail.com>
2023-11-27 03:48:16 +0200

    Init

---
+++ lib/custom_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final Text child;
+  final VoidCallback onPressed;
+  final VoidCallback? onLongPress;
+  final double height;
+  final double width;
+  final Color? color;
+
+  const CustomButton({
+    super.key,
+    required this.child,
+    required this.onPressed,
+    this.onLongPress,
+    this.height = 80,
+    this.width = 90,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: ElevatedButton(
+         style: ElevatedButton.styleFrom(
+           backgroundColor: color ?? Theme.of(context).colorScheme.primary,
+         ),
+        onPressed: onPressed,
+        onLongPress: onLongPress,
+        child: DefaultTextStyle(
+          style: const TextStyle(
+            fontSize: 30,
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
